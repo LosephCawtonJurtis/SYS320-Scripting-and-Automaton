dc40c8587b17e23a15684c2e3b1cd0fd1cce560b
Joseph Lawton-Curtis <joseph.lawtoncurtis@mymail.champlain.edu>
2021-03-20 14:57:09 -0400

    inital week 8 commit

---
+++ Week08/EventLogs.ps1
@@ -0,0 +1,15 @@
+﻿# storyline: Review the Security Event Log
+
+# List all the availabvle windows event logs
+Get-EventLog -list
+
+# Create a prompt to allow user to select the Log to view
+
+$readLog = Read-host -Prompt "Please select a log to review from the list above."
+
+$readSearch = Read-Host -Prompt "include a term to search the log for."
+
+
+# print the results of the log
+Get-EventLog $readLog -Newest 40 | where {$_.Message -ilike "*$readSearch*" } | Export-Csv -NoTypeInformation `
+-Path "A:\School\Classes\sys320\PSdirectory\SYS320-Scripting-and-Automaton\Week08\securityLogs.csv"
---
+++ Week08/autoMail.ps1
@@ -0,0 +1,16 @@
+# Storyline: send an emial.
+
+# Body of email
+# variables can have an underscore or any alphanumeric value.
+$msg = "Hello there."
+
+# echoing to the screen.
+write-host -BackgroundColor Red -ForegroundColor white $msg
+# Email From Address
+$email = "joseph.lawtoncurtis@mymail.champlain.edu"
+
+# to address
+$toEmail = "deployer@csi-web"
+
+# sending the email
+Send-MailMessage -From $email -to $toEmail -Subject "A Greeting" -body $msg -SmtpServer 192.168.4.22
